ffc64edec5f7807ea1f65f033e68ab064ba9258d
Sangram Barge <sangrambarge098@gmail.com>
2024-05-06 09:22:03 +0530

    initial commit

---
+++ src/test/java/dev/sangram/mmanage/MmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sangram.mmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
